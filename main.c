b815165c3e1f73cc3f331ded9b0dbc81e70f144d
Csókás, Bence <bence98@sch.bme.hu>
2024-07-18 23:22:30 +0200

    main: Use getopt for options parsing

--- main.c
+++ main.c
@@ -5,10 +5,12 @@
  */
 
 #include <errno.h>
+#include <getopt.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 #define FLIST_READ
 #include "filelist.h"
@@ -37,10 +39,40 @@ static int cb_do_cat(FILE *f, void *userdata)
 	return do_cat(f, *((bool *)userdata));
 }
 
+static void print_help(const char *prog)
+{
+	fprintf(stderr, "Usage: %s [opts...] [FILEs...]\n\n"
+		"'FILEs' is zero or more input files. "
+		"These will(/won't) be concatenated. "
+		"If no files are given, read from the standard "
+		"input. Additionally, '-' may be specified "
+		"among the files as a synonym for the standard "
+		"input.\n"
+		"Opts:\n"
+		"\t-s/--seed [seed] \t- the seed for the PRNG\n"
+		"", prog);
+}
+
+static const struct option opts[] = {
+	{ "help", 0, NULL, 'h' },
+	{ "seed", 1, NULL, 's' },
+	{ }
+};
+
 int main(int argc, char* argv[])
 {
 	srand(time(NULL));
-	int err;
+	int err, opt;
+
+	while ((opt = getopt_long(argc, argv, "hs:", opts, NULL)) != -1)
+		switch (opt) {
+		case 's':
+			srand(atoi(optarg));
+			break;
+		default:
+			print_help(argv[0]);
+			return (opt == 'h') ? 0 : -EINVAL;
+		}
 
 	bool doPrint = rand() < (RAND_MAX * 0.5);
 
@@ -50,7 +82,7 @@ int main(int argc, char* argv[])
 		return -ENOMEM;
 	}
 
-	for (int i = 1; i < argc; i++) {
+	for (int i = optind; i < argc; i++) {
 		err = flist_add(l, argv[i]);
 		if (err)
 			return err;
